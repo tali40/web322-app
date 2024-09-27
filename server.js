646269581a7fbb010b40b36c892b9858a8b12186
tali40 <taheriali2420@gmail.com>
2024-09-27 16:28:34 -0400

    Update server.js

--- server.js
+++ server.js
@@ -3,10 +3,10 @@ WEB322 – Assignment 02
 I declare that this assignment is my own work in accordance with Seneca Academic Policy.  
 No part of this assignment has been copied manually or electronically from any other source (including 3rd party web sites) or distributed to other students.
 
-Name: ali 
-Student ID: taheri
+Name: ali taheri
+Student ID: 163955230
 Date: 24-09-27
-Vercel Web App URL: https://web322-app-blush.vercel.app/
+Vercel Web App URL: https://webws.vercel.app/about
 GitHub Repository URL: https://github.com/tali40/web322-app
 
 ********************************************************************************/ 
